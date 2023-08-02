d87898d4216d6951851985aa43f0a6830881950e
Tian Gao <gaogaotiantian@hotmail.com>
2023-08-02 12:13:24 -0700

    Add activity feature

--- web/discordbot/admin.py
+++ web/discordbot/admin.py
@@ -1,10 +1,17 @@
-import discord
 from discord.ext import commands
 
 from .bot import bot
+from .firebase_proxy import fb_activity_clear
 
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def synccommands(ctx):
     new_commands = await bot.tree.sync()
     await ctx.send(f"Synced commands - {new_commands}")
+
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def clearactivities(ctx):
+    fb_activity_clear()
+    await ctx.send(f"活动清除成功")
--- web/discordbot/firebase_proxy.py
+++ web/discordbot/firebase_proxy.py
@@ -1,6 +1,7 @@
 import discord
 from firebase_admin import db
 
+
 def fb_get(member: discord.Member, attribute: str):
     ref = db.reference(f"/jx3/users/{member.id}")
     data = ref.get()
@@ -10,8 +11,25 @@ def fb_get(member: discord.Member, attribute: str):
     else:
         return None
 
+
 def fb_set(member: discord.Member, attribute: str, val):
     ref = db.reference(f"/jx3/users/{member.id}")
     ref.update({
         attribute: val
     })
+
+
+def fb_activity_clear():
+    ref = db.reference(f"/jx3/activities")
+    ref.delete()
+
+
+def fb_activity_set(activity_id: str, val):
+    ref = db.reference(f"/jx3/activities/{activity_id}")
+    ref.set(val)
+
+
+def fb_activity_get(activity_id: str):
+    ref = db.reference(f"/jx3/activities/{activity_id}")
+    ret = ref.get()
+    return ret
